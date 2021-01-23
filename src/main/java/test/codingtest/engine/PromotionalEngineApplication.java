430e1743469e634ec7902920f81464c173d44655
Manasi Desai <manasi.desai@riomed.com>
2021-01-23 13:02:22 +0000

    Committing empty project

---
+++ src/main/java/test/codingtest/engine/PromotionalEngineApplication.java
@@ -0,0 +1,13 @@
+package test.codingtest.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionalEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionalEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/codingtest/engine/PromotionalEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package test.codingtest.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionalEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
